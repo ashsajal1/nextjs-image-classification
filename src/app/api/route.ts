5aacfac65e22c1bcb1e9b03ae09f9be8dc88af6b
ashsajal1 <ashsajal1@gmail.com>
2024-01-04 00:06:50 +0800

    Update : Server respondig with image data

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,31 +1,50 @@
 import { NextRequest, NextResponse } from "next/server";
-import fs from "fs";
-
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
 
 export async function POST(req: NextRequest) {
-  const formData = await req.formData();
-  console.log(formData.get("image"));
-  async function query(filename: string) {
-    const data = fs.readFileSync(filename);
-    const response = await fetch(
-      process.env.HUGGING_FACE_MODEL as string,
-      {
-        headers: { Authorization: process.env.HUGGING_FACE_KEY as string },
-        method: "POST",
-        body: data,
-      }
-    );
+  async function query(image: File) {
+    const response = await fetch(process.env.HUGGING_FACE_MODEL as string, {
+      headers: { 
+        'Authorization': process.env.HUGGING_FACE_KEY as string,
+      },
+      method: "POST",
+      body: image,
+    });
+
     const result = await response.json();
     return result;
   }
 
-  //   const result = await query("cats.jpg");
-  return NextResponse.json({
-    ok: true,
-  });
+  try {
+    const formData = await req.formData();
+    console.log(formData.get("image"));
+    const image = formData.get("image");
+    // console.log(image?.name);
+
+    if (!image) {
+      console.log("no image found")
+      return NextResponse.json(
+        { error: "Image is required." },
+        { status: 400 }
+      );
+    }
+
+    const result = await query(image as File);
+
+    console.log(result)
+    
+    return NextResponse.json({
+      ok: true,
+      result,
+    });
+
+  } catch (error) {
+    console.log("first")
+    return NextResponse.json(
+      {
+        error,
+        message:"Error occured"
+      },
+      { status: 400 }
+    );
+  }
 }
