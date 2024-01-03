bb96a9f55a8ebdb58f76fd4bb0a38030abd7aee1
ashsajal1 <ashsajal1@gmail.com>
2024-01-03 20:07:29 +0800

    Setup route

---
+++ src/app/api/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest } from "next/server";
+import fs from "fs";
+
+export async function POST(req: NextRequest) {
+  const { filename } = await req.json();
+  async function query(filename : string) {
+    const data = fs.readFileSync(filename);
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/google/vit-base-patch16-224",
+      {
+        headers: { Authorization: process.env.HUGGING_FACE_KEY as string },
+        method: "POST",
+        body: data,
+      }
+    );
+    const result = await response.json();
+    return result;
+  }
+
+  query("cats.jpg").then((response) => {
+    console.log(JSON.stringify(response));
+  });
+}
