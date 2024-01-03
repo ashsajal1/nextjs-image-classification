7741efdbcba5c7fe0a91d8dc5142f7c3d524c176
ashsajal1 <ashsajal1@gmail.com>
2024-01-03 23:31:26 +0800

    Upadate : File uploaded successfully to server via formdata

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,12 +1,19 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import fs from "fs";
 
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
 export async function POST(req: NextRequest) {
-  const { filename } = await req.json();
+  const formData = await req.formData();
+  console.log(formData.get("image"));
   async function query(filename: string) {
     const data = fs.readFileSync(filename);
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/google/vit-base-patch16-224",
+      process.env.HUGGING_FACE_MODEL as string,
       {
         headers: { Authorization: process.env.HUGGING_FACE_KEY as string },
         method: "POST",
@@ -17,8 +24,8 @@ export async function POST(req: NextRequest) {
     return result;
   }
 
-  const result = await query("cats.jpg");
-  Response.json({
-    result,
+  //   const result = await query("cats.jpg");
+  return NextResponse.json({
+    ok: true,
   });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,48 @@
-import Image from 'next/image'
+'use client';
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    setSelectedFile(file || null);
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (selectedFile) {
+      console.log(selectedFile);
+
+      try {
+        const formData = new FormData();
+        formData.append("image", selectedFile);
+
+        const response = await fetch("/api", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (response.ok) {
+          console.log("Photo uploaded successfully");
+        } else {
+          console.log("Failed to upload photo");
+        }
+      } catch (error) {
+        console.log("An error occurred:", error);
+      }
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form action="" className='flex border p-6 rounded'>
-        <input type='file' placeholder='Upload jpg file' />
-        <button className='rounded p-2 bg-blue-700 text-slate-50' type='submit'>Submit</button>
+      <form action="" className="flex border p-6 rounded">
+        <input onChange={handleFileChange} type="file" placeholder="Upload jpg file" />
+        <button onClick={handleSubmit} className="rounded p-2 bg-blue-700 text-slate-50" type="submit">
+          Submit
+        </button>
       </form>
     </main>
-  )
+  );
 }
